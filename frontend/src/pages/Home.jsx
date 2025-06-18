577bcf83d2b4af56ee2f7f206d8cd9520c6f5aa7
Shubham Dhama <sdhama380@gmail.com>
2025-06-19 02:57:13 +0530

    Add new pages and enhance styling for Contact and About sections

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,21 @@
-import React, { useRef } from 'react'
-import './Home.css'
-import Navbar from '../components/Navbar'
-import { FaArrowRight } from 'react-icons/fa';
-import Form from '../components/Form'
-import Badges from './Badges'
-import Shop from './Shop'
-import Choose from './Choose'
-import Testimonials from './Testimonials';
-import Clients from './Clients';
-import Contact from './Contact';
+import React, { useRef } from "react";
+import "./Home.css";
+import Navbar from "../components/Navbar";
+import { FaArrowRight } from "react-icons/fa";
+import Form from "../components/Form";
+import Badges from "./Badges";
+import Shop from "./Shop";
+import Choose from "./Choose";
+import Testimonials from "./Testimonials";
+import Clients from "./Clients";
+import Contact from "./Contact";
 const Home = () => {
   const chooseRef = useRef(null);
 
   const scrollToChoose = () => {
-    chooseRef.current?.scrollIntoView({ 
-      behavior: 'smooth',
-      block: 'start'
+    chooseRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
     });
   };
 
@@ -23,25 +23,28 @@ const Home = () => {
     <div>
       <div className="home">
         <div className="main-head">
-            <h1 id="head1">Powering Your Fleet With Trusted Used</h1>
-            <h1 id='head2'>HEAVY TRUCK PARTS</h1>
-            <h1 id='head3'>Fueled by <span id='spn'>EXPERTISE. </span>Driven by <span id='spn'>VALUES.</span> Powered by <span id='spn'>TRUST.</span></h1>
+          <h1 id="head1">Powering Your Fleet With Trusted Used</h1>
+          <h1 id="head2">HEAVY TRUCK PARTS</h1>
+          <h1 id="head3">
+            Fueled by <span id="spn">EXPERTISE. </span>Driven by{" "}
+            <span id="spn">VALUES.</span> Powered by{" "}
+            <span id="spn">TRUST.</span>
+          </h1>
         </div>
         <div className="arrow-button" onClick={scrollToChoose}>
-      <FaArrowRight className="arrow-icon" />
-    </div>
-    <Form />
+          <FaArrowRight className="arrow-icon" />
+        </div>
+        <Form />
       </div>
       <Badges />
       <Shop />
       <div ref={chooseRef}>
-        <Choose/>
+        <Choose />
       </div>
       <Testimonials />
       <Clients />
-      <Contact />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
