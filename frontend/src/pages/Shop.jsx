7708d1edc0cb709761169dd7578df0a885bd9c7b
Amit Raj <amitraj4381@gmail.com>
2025-07-18 02:27:54 +0530

    updated gtag and privacy

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Shop.css'
 import ShopCard from '../components/ShopCard';
 import engine from '../assets/engine.png';
@@ -6,17 +7,29 @@ import trans from '../assets/transmission.png';
 import parts from '../assets/parts.png';
 
 const Shop = () => {
+  const handleLinkClick = () => {
+    // Small delay to allow navigation to complete
+    setTimeout(() => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }, 100);
+  };
+
   return (
     <>
-        <div id="shop">
-            <h1 id='shead'>SHOP BY CATEGORY</h1>
+      <div id="shop">
+        <h1 id='shead'>SHOP BY CATEGORY</h1>
         <div id="cards">
+          <Link to="/engine" style={{ textDecoration: 'none' }} onClick={handleLinkClick}>
             <ShopCard imageSrc={engine} label="TRUCK ENGINE" />
+          </Link>
+          <Link to="/transmission" style={{ textDecoration: 'none' }} onClick={handleLinkClick}>
             <ShopCard imageSrc={trans} label="TRUCK TRANSMISSION" />
+          </Link>
+          <Link to="/" style={{ textDecoration: 'none' }} onClick={handleLinkClick}>
             <ShopCard imageSrc={parts} label="TRUCK PARTS" />
-
+          </Link>
         </div>
-        </div>
+      </div>
     </>
   )
 }
