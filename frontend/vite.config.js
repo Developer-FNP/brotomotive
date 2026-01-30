5b965a2e9cedc0ad7fde520770491bc9b42ce86d
Shubham Dhama <shubhamdhama@Shubhams-Mac-mini.local>
2026-01-31 01:30:23 +0530

    vite  config fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: import.meta.env.PORT ? parseInt(import.meta.env.PORT) : 4173,
+    strictPort: false,
+  }
 })
